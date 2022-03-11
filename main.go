e550abf4df336262b59978a985e869157fe47727
Javier <48791471+orov-io@users.noreply.github.com>
2022-03-11 22:55:28 +0100

    Develop (#9)
    * Boilerplate
    
    * Add endpoint to create a new recipe
    
    * Add recipe data model
    
    * Add endpoint to create a new recipe
    
    Co-authored-by: Javi <javier.coronel@triporate.es>
    
    * List all recipes (#4)
    
    Co-authored-by: Javi <javier.coronel@triporate.es>
    
    * Add update recipe capability (#5)
    
    Co-authored-by: Javi <javier.coronel@triporate.es>
    
    * Add delete recipe capability (#6)
    
    Co-authored-by: Javi <javier.coronel@triporate.es>
    
    * Add search recipe by tag
    
    * Add delete recipe capability
    
    * Add search recipe by tag
    
    Co-authored-by: Javi <javier.coronel@triporate.es>
    
    * Update go mod
    
    * Add openapi specs (#8)
    
    Co-authored-by: Javi <javier.coronel@triporate.es>
    
    Co-authored-by: Javi <javier.coronel@triporate.es>

--- main.go
+++ main.go
@@ -1,12 +1,33 @@
+// Recipes API
+//
+// This is a sample recipes API.
+//
+// 	Schemes: http
+// 	Host: localhost:8080
+// 	BasePath: /
+// 	Version: 1.0.0
+// 	Contact: Javi<hi@orov.io>
+// 	Consumes:
+// 		- application/json
+//
+// 	Produces:
+// 		- application/json
+// swagger:meta
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
 )
 
 type Recipe struct {
+	ID           string    `json:"id"`
 	Name         string    `json:"name"`
 	Tags         []string  `json:"tags"`
 	Ingredients  []string  `json:"ingredients"`
@@ -14,7 +35,173 @@ type Recipe struct {
 	PublishedAt  time.Time `json:"publishedAt"`
 }
 
+var recipes []Recipe
+
+func init() {
+	recipes = make([]Recipe, 0)
+	file, _ := ioutil.ReadFile("recipes.json")
+	_ = json.Unmarshal([]byte(file), &recipes)
+}
+
+// swagger:operation POST /recipes recipes newRecipe
+// Create a new recipe
+// ---
+// produces:
+// - application/json
+// responses:
+//     '200':
+//         description: Successful operation
+//     '400':
+//         description: Invalid input
+func NewRecipe(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+}
+
+// swagger:operation GET /recipes recipes listRecipes
+// Returns list of recipes
+//
+// ---
+// produces:
+// - application/json
+// responses:
+//   '200':
+//     description: Successful operation
+func ListRecipes(c *gin.Context) {
+	c.JSON(http.StatusOK, recipes)
+}
+
+// swagger:operation PUT /recipes/{id} recipes updateRecipe
+// Update an existing recipe
+// ---
+// parameters:
+// - name: id
+//   in: path
+//   description: Id of the recipe
+//   required: true
+//   type: string
+// produces:
+// - application/json
+// responses:
+//   '200':
+//     description: Successful operation
+//   '400':
+//     description: Invalid input
+//   '200':
+//     description: Invalid recipe ID
+func UpdateRecipe(c *gin.Context) {
+	id := c.Param("id")
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+			break
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+
+	recipe.ID = id
+	recipes[index] = recipe
+	c.JSON(http.StatusOK, recipe)
+}
+
+// swagger:operation DELETE /recipes/{id} recipes deleteRecipe
+// Delete an existing recipe
+// ---
+// produces:
+// - application/json
+// parameters:
+//   - name: id
+//     in: path
+//     description: ID of the recipe
+//     required: true
+//     type: string
+// responses:
+//     '200':
+//         description: Successful operation
+//     '404':
+//         description: Invalid recipe ID
+func DeleteRecipe(c *gin.Context) {
+	id := c.Param("id")
+
+	index := -1
+	for i := 0; i < len(recipes); i++ {
+		if recipes[i].ID == id {
+			index = i
+			break
+		}
+	}
+
+	if index == -1 {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Recipe not found",
+		})
+		return
+	}
+
+	recipes = append(recipes[:index], recipes[index+1:]...)
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Recipe has been deleted",
+	})
+}
+
+// swagger:operation GET /recipes/search recipes findRecipe
+// Search recipes based on tags
+// ---
+// produces:
+// - application/json
+// parameters:
+//   - name: tag
+//     in: query
+//     description: recipe tag
+//     required: true
+//     type: string
+// responses:
+//     '200':
+//         description: Successful operation
+func SearchRecipes(c *gin.Context) {
+	tag := c.Query("tag")
+	matchedRecipes := make([]Recipe, 0)
+	for i := 0; i < len(recipes); i++ {
+		for _, t := range recipes[i].Tags {
+			if strings.EqualFold(t, tag) {
+				matchedRecipes = append(matchedRecipes, recipes[i])
+				break
+			}
+		}
+	}
+	c.JSON(http.StatusOK, matchedRecipes)
+}
+
 func main() {
 	router := gin.Default()
+	router.POST("/recipes", NewRecipe)
+	router.GET("/recipes", ListRecipes)
+	router.PUT("/recipes/:id", UpdateRecipe)
+	router.DELETE("/recipes/:id", DeleteRecipe)
 	router.Run()
 }
